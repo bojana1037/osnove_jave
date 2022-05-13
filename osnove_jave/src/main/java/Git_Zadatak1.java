ae30b0a9b4d74758aa90207cf18e8508cd8a840a
Bojana Stojanovic <booojana@gmail.com>
2022-05-13 20:48:44 +0200

    Predavanja 13.05 Git Zadatak 1

---
+++ osnove_jave/src/main/java/Git_Zadatak1.java
@@ -0,0 +1,9 @@
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("git add");
+        System.out.println("git clear");
+        System.out.println("git status");
+        System.out.println("git clone");
+        System.out.println("git commit");
+    }
+}
